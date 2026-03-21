6b9dc0842637b8386e1dec5d91fba3e36564d2d6
MysticBerry7 <marlibester101@gmail.com>
2026-03-21 13:33:16 +0200

    item id

--- myapp/components/search_for_materials.py
+++ myapp/components/search_for_materials.py
@@ -1,6 +1,10 @@
 from django_unicorn.components import UnicornView
 from random import randint
 
+# Useful comments to remember
+# git commit -am "comment"
+# git push origin master
+
 class SearchForMaterialsView(UnicornView):
     rocks = 0  # Declare all materials, sorted in common, rare, and the berry
     leaves = 0
@@ -18,6 +22,21 @@ class SearchForMaterialsView(UnicornView):
     if item_det < 4:
         common_item_id = randint(0, 4)
         if common_item_id == 0:
-
+            rocks += 1
+        elif common_item_id == 1:
+            leaves += 1
+        elif common_item_id == 2:
+            twigs += 1
+        elif common_item_id == 3:
+            bird_eggs += 1
+        else:
+            antlers += 1
     else:
+        rare_item_id = randint(0, 2)
+        if rare_item_id == 0:
+            beetles += 1
+        elif rare_item_id == 1:
+            quartz += 1
+        else:
+            frog_legs += 1
     
